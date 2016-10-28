f18299a4ad65dff3e6ab86d38210379d79325f68
Elena <Olga@MacBook-Air-Elena.local>
2016-10-28 14:59:19 +0300

    Extend Product model, add description and price

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161028110333) do
+ActiveRecord::Schema.define(version: 20161028112824) do
 
   create_table "products", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
+    t.float    "price"
   end
 
   create_table "users", force: :cascade do |t|
